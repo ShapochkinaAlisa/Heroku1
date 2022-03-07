0ad39a6625c621c22d950e5b42649f6797ee20e7
Алиса <alisashapochkina@mail.ru>
2022-03-07 18:20:55 +0400

    марс

--- AAA/data/users.py
+++ AAA/data/users.py
@@ -1,10 +1,11 @@
 import datetime
 import sqlalchemy
 from sqlalchemy import orm
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from .db_session import SqlAlchemyBase
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
@@ -21,3 +22,12 @@ class User(SqlAlchemyBase):
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
     jobs = orm.relation("Jobs", back_populates='user')
+
+    def repr(self):
+        return f'<Colonist> {self.id} {self.surname} {self.name}'
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- AAA/main111.py
+++ AAA/main111.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
+from flask_login import login_user, LoginManager
 
+from AAA.data.login_form import LoginForm
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    db_session.global_init("db/mars.db")
+    db_session.global_init("db/mars_exp.db")
     app.run()
 
 
@@ -21,8 +25,31 @@ def index():
     users = db_sess.query(User).all()
     names = {name.id: (name.surname, name.name) for name in users}
     print(names)
-    return render_template("index.html", jobs=jobs, users=names)
+    return render_template("index.html", jobs=jobs, names=names)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        user.set_password('123')
+        print(user.hashed_password)
+        print(user)
+        print(form.password.data)
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 if __name__ == '__main__':
     main()
